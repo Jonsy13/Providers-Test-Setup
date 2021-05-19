10d73960c3bdb2c6d0a61813af0366bfa91067b4
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-05-19 10:59:41 +0530

    Added Portal Setup step
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

---
+++ Cypress/cypress/integration/Parallel_Tests/Create_Workflow/workflow.spec.js
@@ -0,0 +1,127 @@
+/// <reference types="Cypress" />
+import * as workflows from "../../../fixtures/Workflows.json";
+import * as user from "../../../fixtures/Users.json";
+
+describe("Testing the create Workflow Utility", () => {
+  before("Clearing the Cookies and deleting the Cookies", () => {
+    cy.requestLogin(user.AdminName, user.AdminPassword);
+  });
+
+  it("Checking the accessibility of the choose-cluster page", () => {
+    // cy.waitForSelfCluster();
+    cy.visit("/create-workflow");
+    cy.url().should("contain", "/create-workflow");
+    cy.get("[data-cy=AgentsRadioGroup]").should("be.visible");
+  });
+
+  it("Checking choose cluster screen functionality", () => {
+    cy.get('[type="radio"]').eq(0).check(); //Selecting Self Cluster from DropDown
+    cy.get("[data-cy=ControlButtons]").children().eq(0).should("be.enabled"); //Only Next Button is there on this Screen
+    cy.get("[data-cy=ControlButtons]").children().eq(0).click();
+    cy.url().should("contain", "/create-workflow");
+  });
+
+  it("Checking Choose Workflow screen", () => {
+    cy.get("[data-cy=WorkflowsRadioGroup]").should("be.visible");
+    cy.get("[data-cy=WorkflowsRadioGroup]").children().eq(0).check();
+    // cy.get("[data-cy=StepperButtons]").should("be.visible");
+    // cy.get("[data-cy=StepperButtons]").children().eq(1).should("be.disabled");
+    // cy.get("[data-cy=StepperButtons]").children().eq(0).should("be.enabled");
+    // cy.get("[data-cy=PredefinedWorkflowsPanel]").children().eq(1).click();
+    // cy.get("[data-cy=EditWorkflowButton]").click();
+    // cy.get("[data-cy=WorkflowNameInput] input")
+    //   .click()
+    //   .clear()
+    //   .type(workflows.nonRecurringworkflowName);
+    // cy.get("[data-cy=WorkflowDescriptionInput] textarea")
+    //   .click()
+    //   .clear()
+    //   .type(workflows.nonRecurringworkflowDescription);
+    // cy.get("[data-cy=WorkflowDetailsModalButtons").children().eq(1).click();
+    // cy.get("[data-cy=StepperButtons]").children().eq(1).should("be.enabled");
+    // cy.get("[data-cy=StepperButtons]").children().eq(1).click();
+    // cy.contains("Tune the selected workflow").scrollIntoView().should("be.visible"); //Not able to come in View
+  });
+
+  // it("Checking the workflow Editor Page", () => {
+  //   cy.get("[data-cy=WorkflowEditor]").should("be.visible");
+  //   cy.get("[data-cy=StepperButtons]").scrollIntoView().should("be.visible");
+  //   cy.get("[data-cy=StepperButtons]")
+  //     .scrollIntoView()
+  //     .children()
+  //     .eq(1)
+  //     .click();
+  // });
+
+  // it("Checking the resiliency score page", () => {
+  //   cy.contains("Adjust the weights of the experiments in the workflow").should(
+  //     "be.visible"
+  //   );
+  //   cy.get("[data-cy=ExperimentWeightSlider]").should("be.visible");
+
+  //   // Code for Slider Manipulation
+  //   cy.get("[data-cy=WeightSlider]").invoke("val", 5).trigger("change").click();
+  //   cy.get("[data-cy=ExperimentWeight]").should("have.text", "5 points");
+  //   cy.get("[data-cy=StepperButtons]").should("be.visible");
+  //   cy.get("[data-cy=StepperButtons]").children().eq(1).click();
+  // });
+
+  // it("Checking the Scheduling options page", () => {
+  //   cy.contains("Choose a chaos schedule").should("be.visible");
+  //   cy.get("[data-cy=ScheduleOptions]").should("be.visible");
+  //   cy.get("[data-cy=ScheduleOptions] :checked")
+  //     .should("be.checked")
+  //     .should("have.value", "now");
+  //   cy.get("[data-cy=StepperButtons]").should("be.visible");
+  //   cy.get("[data-cy=StepperButtons]").children().eq(1).click();
+  // });
+
+  // it("Checking Verify Screen", () => {
+  //   cy.contains("Confirmation of Results").should("be.visible");
+  //   console.log(cy.get("[data-cy=WorkflowName]"));
+  //   cy.get("[data-cy=StepperButtons]").children().eq(1).click();
+  //   cy.get("[data-cy=FinishModal]").should("be.visible");
+  //   cy.get("[data-cy=GoToWorkflowButton]").click();
+  // });
+
+  // it("Checking Workflow Browsing Table for scheduled workflow", () => {
+  //   cy.get("[data-cy=WorkflowRunsTable]", { timeout: 30000 }).should(
+  //     "be.visible"
+  //   );
+  //   cy.get("[data-cy=WorkflowRunsTableRow]")
+  //     .eq(0)
+  //     .children()
+  //     .eq(0)
+  //     .should("have.text", "Running");
+  //   cy.get("[data-cy=WorkflowRunsTableRow]")
+  //     .eq(0)
+  //     .children()
+  //     .eq(1)
+  //     .should("have.text", workflows.nonRecurringworkflowName);
+  //   cy.get("[data-cy=WorkflowRunsTableRow]")
+  //     .eq(0)
+  //     .children()
+  //     .eq(2)
+  //     .should("have.text", "Self-Cluster");
+  // });
+
+  // it("Checking Schedules Table for scheduled Workflow", () => {
+  //   cy.get("[data-cy=browseSchedule]").click();
+  //   cy.get("[data-cy=workflowSchedulesTable]").should("be.visible");
+  //   cy.get("[data-cy=workflowSchedulesTableRow]")
+  //     .eq(0)
+  //     .children()
+  //     .eq(0)
+  //     .should("have.text", workflows.nonRecurringworkflowName);
+  //   cy.get("[data-cy=workflowSchedulesTableRow]")
+  //     .eq(0)
+  //     .children()
+  //     .eq(2)
+  //     .should("have.text", "Once");
+  //   cy.get("[data-cy=workflowSchedulesTableRow]")
+  //     .eq(0)
+  //     .children()
+  //     .eq(3)
+  //     .should("have.text", "Self-Cluster");
+  // });
+});
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Providers-Test-Setup
-Repository contains Automation scripts for Cluster Setup and Teardown on different providers
+## litmus-portal-e2e
+
+This branch contains Gitlab e2e pipeline for litmus-portal with BDD tests using Cypress.
+
+# Here are the different stages of the Gitlab pipeline:
+
+<table>
+  <tr>
+    <th>Stages</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>Setup</td>
+    <td>This stage is used for creating a 3-node cluster on EKS.</td>
+  </tr>
+  <tr>
+    <td>Portal-Setup</td>
+    <td>This stage is used for deploying the litmus-portal on 3-node cluster created in Setup stage and also verifies if all pods and services are ready or not.</td>
+  </tr>
+  <tr>
+    <td>Test-Setup</td>
+    <td>This stage is used for installing Cypress for first time and warming up the cached dependencies of Cypress.</td>
+  </tr>
+  <tr>
+    <td>Pre-Test-Setup</td>
+    <td>This stage is used for doing pre-test checkups such as login and welcome-modal functionalities.</td>
+  </tr>
+  <tr>
+    <td>Cypress-Test</td>
+    <td>This stage is used for testing different functionalities of litmus-portal such as User-management, Creation of workflows and browsing of workflows,etc</td>
+  </tr>
+  <tr>
+    <td>Portal-Cleanup</td>
+    <td>This stage is used for deleting the all resources of litmus-portal from the cluster.</td>
+  </tr>
+  <tr>
+    <td>Cluster Cleanup</td>
+    <td>In this stage, the 3-node cluster is distroyed.</td>
+  </tr>
+</table>
