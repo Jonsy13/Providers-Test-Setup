448deecbd15039fe02e23e6ca912bf21103a6357
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-05-19 10:20:03 +0530

    Added Portal Setup step
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- litmus/setup.sh
+++ litmus/setup.sh
@@ -5,6 +5,9 @@ path=$(pwd)
 # # Setting up the kubeconfig
 mkdir -p ~/.kube
 
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+
 cp $path/.kube/config ~/.kube/config
 cp $path/.kube/admin.conf ~/.kube/config
 
